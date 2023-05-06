f1cb017e531439a730290d74bec6149bc08e5090
Anthony Tleiji <anthonytleiji@gmail.com>
2023-05-06 15:14:03 +0300

    Minor dto change.

--- src/task/dto/import-result.dto.ts
+++ src/task/dto/import-result.dto.ts
@@ -18,15 +18,16 @@ export interface FailTask {
 }
 
 export interface ImportConversionOptions {
-  idColName: string;
-  titleColName: string;
-  managerColName: string;
-  assigneeColName: string;
-  statusColName: string;
+  idColName?: string;
+  titleColName?: string;
+  managerColName?: string;
+  assigneeColName?: string;
+  statusColName?: string;
 }
 
 export interface ImportOptions extends ImportConversionOptions {
   handleErrors?: boolean;
+  worksheetName?: string;
 }
 
 export interface ImportResults {
